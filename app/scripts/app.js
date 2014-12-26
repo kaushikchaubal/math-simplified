205b6ab82ad46d408d997ff0f2197f95fd937c53
Kaushik Chaubal <kaushikchaubal@gmail.com>
2014-12-26 11:34:31 +0000

    First Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('math-simplified', [
+    'learn',
+    'discuss',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/views/main.html'
+      })
+      .when('/learn', {
+        templateUrl: 'app/views/learn.html'
+      })
+      .when('/discuss', {
+        templateUrl: 'app/views/construction.html'
+      })
+      .when('/about', {
+        templateUrl: 'app/views/about.html'
+      })
+      .when('/contact', {
+        templateUrl: 'app/views/construction.html'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/learn.js
@@ -0,0 +1,92 @@
+(function() {
+  var app = angular.module('learn', []);
+
+  app.controller('StoreController', function() {
+    this.products = courseList;
+  });
+
+  app.controller("TabController", function() {
+    this.tab = 1;
+
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
+    };
+
+    this.setTab = function(setTab) {
+      this.tab = setTab;
+    };
+  });
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
+
+  app.controller("ReviewController", function(){
+
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+  var courseList = [{
+      name: 'Basic Arthimatic',
+      description: "This course goes through the basic concepts of arthimatic",
+      topics: "addition, subtraction, multiplication, division",
+      level: "Basic",
+      reviews: [{
+        stars: 5,
+        body: "I love this course!",
+        author: "joe@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 2,
+        body: "This course was too basic",
+        author: "tim@example.org",
+        createdOn: 1397490980837
+      }]
+    }, {
+      name: 'Quadratic Equations',
+      description: "Equations can be fun to solve - lets do some fun euqations!",
+      topics: "Everything you want about equations!",
+      level: "Intermediate",
+      reviews: [{
+        stars: 3,
+        body: "I think this course was just OK, could honestly use more hands-on experience",
+        author: "JimmyDean@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 4,
+        body: "This course was PERFECT for me!",
+        author: "gemsRock@example.org",
+        createdOn: 1397490980837
+      }]
+    }, {
+      name: 'Trignometry',
+      description: "This topic covers the fundmantels of Trignometry",
+      topics: "Some triangles related thing!",
+      level: "Advanced",
+      reviews: [{
+        stars: 1,
+        body: "This course is WAY too in-depth for its rarity value.",
+        author: "turtleguyy@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 4,
+        body: "Oh my! I love fund management after this!",
+        author: "LouisW407@example.org",
+        createdOn: 1397490980837
+      }, {
+        stars: 5,
+        body: "This feels like the course I always was looking for",
+        author: "nat@example.org",
+        createdOn: 1397490980837
+      }]
+    }];
+})();
