aef66d08cae6173089bd06191babdc2120502b4b
Kaushik Chaubal <kaushikchaubal@gmail.com>
2014-12-28 11:08:02 +0000

    Connecting to mongodb

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 learnController = require('./server/controllers/learnController');
+
+mongoose          = require('mongoose'),
+mongoose.connect('mongodb://math-admin:math-admin@ds043210.mongolab.com:43210/heroku_app32851124');
  
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname));
