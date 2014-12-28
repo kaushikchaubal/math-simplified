900e485bac0faae15d01195070ba7ceabbf3d74f
Kaushik Chaubal <kaushikchaubal@gmail.com>
2014-12-28 11:29:00 +0000

    web.js clean-up

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var morgan = require('morgan');
+var mongoose = require('mongoose');
+var learnController = require('./server/controllers/learnController');
+
+// Start app as an express-app on a particular port
 var app = express();
-learnController = require('./server/controllers/learnController');
-
-mongoose          = require('mongoose'),
-mongoose.connect('mongodb://math-admin:math-admin@ds043210.mongolab.com:43210/heroku_app32851124');
- 
-app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname));
-
-app.get('/api/getCourseList', learnController.courseList);
-
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Listening at port %d', port);
+
+// Setup the satic content using gzippo
+app.use(gzippo.staticGzip("" + __dirname));
+
+// Connect to MongoDB remotely
+mongoose.connect('mongodb://math-admin:math-admin@ds043210.mongolab.com:43210/heroku_app32851124');
+
+// Expose APIs
+app.get('/api/getCourseList', learnController.courseList);
+
